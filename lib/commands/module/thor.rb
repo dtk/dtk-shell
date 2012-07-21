969b3168f7b80a3eb61beea27ef13ea448bbb802
root <root@ip-10-212-139-244.ec2.internal>
2012-07-21 05:06:45 +0000

    added interface to remove direct access

--- lib/commands/module/thor.rb
+++ lib/commands/module/thor.rb
@@ -43,6 +43,18 @@ module DTK::Client
       }
       post rest_url("component_module/add_user_direct_access"), post_body
     end
+    desc "remove-direct-access [PATH-TO-RSA-PUB-KEY]","Removes direct access to modules. Optional paramaeters is path to a ssh rsa public key and default is <user-home-dir>/.ssh/id_rsa.pub"
+    def remove_direct_access(path_to_key=nil)
+      path_to_key ||= "#{ENV['HOME']}/.ssh/id_rsa.pub" #TODO: very brittle
+      unless File.file?(path_to_key)
+        raise Error.new("No File found at (#{path_to_key}). Path is wrong or it is necessary to generate the public rsa key (e.g., run ssh-keygen -t rsa)")
+      end
+      rsa_pub_key = File.open(path_to_key){|f|f.read}
+      post_body = {
+        :rsa_pub_key => rsa_pub_key.chomp
+      }
+      post rest_url("component_module/remove_user_direct_access"), post_body
+    end
 
 
     desc "update-library COMPONENT-MODULE-ID", "Updates library module with workspace module"
