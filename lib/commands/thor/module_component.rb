7d625c86540d2d842a82999a355f12d8cc640d88
Richard Pelavin <rich.pelavin@gmail.com>
2012-09-13 01:03:22 +0000

    moving add or remove direct acccess (temporaily) to module-component commmands

--- lib/commands/thor/module_component.rb
+++ lib/commands/thor/module_component.rb
@@ -123,6 +123,37 @@ module DTK::Client
       response
     end
 
+    #TODO: add-direct-access and remove-direct-access should be removed as commands and instead add-direct-access shoudl be done at initialization time depending on config options"
+    desc "add-direct-access [PATH-TO-RSA-PUB-KEY]","Adds direct access to modules. Optional paramaeters is path to a ssh rsa public key and default is <user-home-dir>/.ssh/id_rsa.pub"
+    def add_direct_access(path_to_key=nil)
+      path_to_key ||= SshProcessing.default_rsa_pub_key_path()
+      unless File.file?(path_to_key)
+        raise DTK::Client::DtkError, "No File found at (#{path_to_key}). Path is wrong or it is necessary to generate the public rsa key (e.g., run ssh-keygen -t rsa)"
+      end
+      rsa_pub_key = File.open(path_to_key){|f|f.read}
+      post_body = {
+        :rsa_pub_key => rsa_pub_key.chomp
+      }
+      response = post(rest_url("component_module/add_user_direct_access"),post_body)
+      return response unless response.ok?
+      repo_manager_footprint,repo_manager_dns = response.data_ret_and_remove!(:repo_manager_footprint,:repo_manager_dns)
+      SshProcessing.update_ssh_known_hosts(repo_manager_dns,repo_manager_footprint)
+      response
+    end
+
+    desc "remove-direct-access [PATH-TO-RSA-PUB-KEY]","Removes direct access to modules. Optional paramaeters is path to a ssh rsa public key and default is <user-home-dir>/.ssh/id_rsa.pub"
+    def remove_direct_access(path_to_key=nil)
+      path_to_key ||= "#{ENV['HOME']}/.ssh/id_rsa.pub" #TODO: very brittle
+      unless File.file?(path_to_key)
+        raise DTK::Client::DtkError,"No File found at (#{path_to_key}). Path is wrong or it is necessary to generate the public rsa key (e.g., run ssh-keygen -t rsa)"
+      end
+      rsa_pub_key = File.open(path_to_key){|f|f.read}
+      post_body = {
+        :rsa_pub_key => rsa_pub_key.chomp
+      }
+      post rest_url("component_module/remove_user_direct_access"), post_body
+    end
+
 
     # we make valid methods to make sure that when context changing
     # we allow change only for valid ID/NAME
