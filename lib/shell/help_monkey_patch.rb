b4922c7370c0b06b12572f183e09cd595a16cda9
Haris KRAJINA <haris@atlantbh.com>
2012-09-24 14:45:40 +0200

    Fix for DTK 211 where we are making sure that help is nto displaying optional content on tier 1 lvl

--- lib/shell/help_monkey_patch.rb
+++ lib/shell/help_monkey_patch.rb
@@ -22,18 +22,36 @@ class Thor
 
         list.each do |help_item|
           # matches identifiers for ID/NAME
-          matched_data = help_item.first.match(/\s\[?#{command}.?(NAME\/ID|ID\/NAME)\]?\s/)
+          matched_data          = help_item.first.match(/\s\[?#{command}.?(NAME\/ID|ID\/NAME)\]?\s/)
+          # if list command matches all bracketed options
+          matched_list_options = help_item.first.match(/list (\[.+\])/)
+          list_optional_data = matched_list_options.nil? ? nil : matched_list_options[1].split(' ')
+
           if matched_data.nil?
             # not found and tier 1 we add it to help list
             filtered_list << help_item if @@shell_context.tier_1?
           else
-            unless @@shell_context.tier_1?
+            if @@shell_context.tier_1?
+              # remove optional data if tier 1 (DTK-211) TODO: Refactor this
+              help_item.first.gsub!(list_optional_data[0],'') unless list_optional_data.nil?
+
+              # if it contains [] it is optional and will be available on both tiers
+              if matched_data[0].include?('[')
+                # we remove it, since there is no need to use it
+                help_item.first.gsub!(matched_data[0],' ') unless help_item.nil?
+                filtered_list << help_item  
+              end
+            else
+              # TODO: Better solution for this type of problems (DTK-211)
+              unless list_optional_data.nil?
+                for i in 1..list_optional_data.size
+                  help_item.first.gsub!(list_optional_data[i].to_s,'')
+                end
+              end
+
               # found and tier 2 we add it to list and remove ID/NAME part of desc
               help_item.first.gsub!(matched_data[0],'') unless help_item.nil?
               filtered_list << help_item
-            else
-               # if it contains [] it is optional and will be available on both tiers
-              filtered_list << help_item  if matched_data[0].include?('[')
             end            
           end
         end
