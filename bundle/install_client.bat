12a79fc1339f2e4d3c5774a6c6d3a7f73edffcfc
Haris KRAJINA <haris@atlantbh.com>
2012-10-08 10:31:34 +0200

    Fixed wrong IP for geminabox

--- bundle/install_client.bat
+++ bundle/install_client.bat
@@ -3,7 +3,7 @@
 
 :: GLOBAL VAR
 
-set abh_gem_repository="http://abh:haris@ec2-176-34-95-163.eu-west-1.compute.amazonaws.com:3000/"
+set abh_gem_repository="http://abh:haris@ec2-54-247-191-95.eu-west-1.compute.amazonaws.com:3000/"
 set log_file="%TMP%\dtk-client.log"
 
 echo "Welcome to DTK CLI Client installation!"
