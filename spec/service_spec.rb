11fcd021da71822fc96d45aabbe7679a401a48eb
Aldin Milanovic <aldin@atlantbh.com>
2012-10-08 15:04:03 +0200

    Some changes in unit tests.

---
+++ spec/service_spec.rb
@@ -0,0 +1,49 @@
+require 'lib/spec_thor'
+dtk_nested_require("../lib/commands/thor","service")
+include SpecThor
+
+describe DTK::Client::Service do
+
+  # generic test for all task of Thor class
+  #test_task_interface(DTK::Client::ServiceModule)
+  lists = ['none', 'assemblies']
+  $module_id = ''
+
+  context "#list" do
+  	command = "dtk service list"
+  	output = `#{command}`
+
+  	it "should list all modules" do
+  		output.should match(/(id|empty|error)/)
+  	end
+
+  	unless output.nil?
+  		$module_id = output.match(/\D([0-9]+)\D/)
+  	end
+  end
+
+  context "#list command" do
+  	unless $module_id.nil?
+  		lists.each do |l|
+  			case l
+  			when 'none'
+  				command = "dtk service #{$module_id} list #{l}"
+  				output  = `#{command}`
+
+  				it "should list all modules" do
+  					output.should match(/(id|empty|error)/)
+  				end
+  			when 'assemblies'
+  				command = "dtk service #{$module_id} list #{l}"
+  				output  = `#{command}`
+
+  				it "should list all assemblies for module with id #{$module_id}" do
+  					output.should match(/(id|empty|error)/)
+  				end
+  			end
+  		end
+  	end
+  end
+
+end
+
