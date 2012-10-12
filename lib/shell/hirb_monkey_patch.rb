35d149c94d81f4afdf28164e357b5f453a8955b4
Haris KRAJINA <haris@atlantbh.com>
2012-10-12 15:33:27 +0200

    Minor fixes to error print

---
+++ lib/shell/hirb_monkey_patch.rb
@@ -0,0 +1,12 @@
+module Hirb
+	class Helpers::ObjectTable
+	  def format_cell(value, cell_width)
+	  	puts "GHOOOOOOOOOOOOOOOOOOOO"
+	    text = String.size(value) > cell_width ?
+	      (
+	      (cell_width < 5) ? String.slice(value, 0, cell_width) : String.slice(value, 0, cell_width - 3) + '...'
+	      ) : value
+	    String.ljust(text, cell_width)
+	  end
+	end
+end
