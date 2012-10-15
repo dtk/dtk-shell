14185dc77c44955f8800eea9cc8e88d181378446
Richard Pelavin <rich.pelavin@gmail.com>
2012-10-15 20:47:06 +0000

    formated lib/util/os_util.rb w/o tabs and substituted Etc.getlogin to handle 'su environemnts' with Common::Aux.running_process_user

--- lib/util/os_util.rb
+++ lib/util/os_util.rb
@@ -1,38 +1,30 @@
-require 'etc'
-
 module DTK
-	module Client
-		module OsUtil
+  module Client
+    module OsUtil
+      def is_mac?
+        RUBY_PLATFORM.downcase.include?('darwin')
+      end
 
-			def is_mac?
-				RUBY_PLATFORM.downcase.include?('darwin')
-			end
+      def is_windows?
+        RUBY_PLATFORM.downcase.include?('mswin')
+      end
 
-			def is_windows?
-				RUBY_PLATFORM.downcase.include?('mswin')
-			end
+      def is_linux?
+        RUBY_PLATFORM.downcase.include?('linux')
+      end
 
-			def is_linux?
-				RUBY_PLATFORM.downcase.include?('linux')
-			end
+      def get_temp_location
+        is_windows? ? ENV['TEMP'] : '/tmp'
+      end
 
-			def get_temp_location
-				if is_windows?
-					return ENV['TEMP']
-				else
-					return '/tmp'
-				end
-			end
-
-			def get_log_location
-				if is_windows?
-					return "#{ENV['APPDATA']}\\DTK"
-				else
-					# returns log_path for current user e.g /var/log/user1
-					return "/var/log/#{Etc.getlogin}"
-				end
-			end
-
-		end
+      def get_log_location
+        if is_windows?
+	  "#{ENV['APPDATA']}\\DTK"
+	else
+	  # returns log_path for current user e.g /var/log/user1
+	  "/var/log/#{Common::Aux.running_process_user()}"
 	end
+      end
+    end
+  end
 end
