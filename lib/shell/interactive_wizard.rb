f71443ecab65a613fe596ecbce7fb5443236ec99
Richard Pelavin <rich.pelavin@gmail.com>
2012-11-02 17:47:47 +0000

    refined shell/interactive_wizard.rb to take out mock and display the display_name info

--- lib/shell/interactive_wizard.rb
+++ lib/shell/interactive_wizard.rb
@@ -12,54 +12,29 @@ module DTK
       
 
       def initialize
-        # TODO Remove this after we get response from server
-        @mock_up_response =   [
-          {
-            "id"=>2147515644,
-            "description"=>"Application directory",
-            "datatype"=>"string",
-            "display_name"=>"server/thin/app_dir"
-          },{
-            "id"=>2147515645,
-            "description"=>"Daemon User",
-            "datatype"=>"string",
-            "display_name"=>"server/thin/daemon_user"
-          },{
-            "id"=>2147515646,
-            "description"=>"User name",
-            "datatype"=>"string",
-            "display_name"=>"server/dtk_server::tenant/name"
-          },{
-            "id"=>2147515648,
-            "description"=>"DB name",
-            "datatype"=>"string",
-            "display_name"=>"server/dtk_postgresql::db/name"
-          },{
-            "id"=>2147515649,
-            "description"=>"Client linux account name",
-            "datatype"=>"string",
-            "display_name"=>"server/gitolite::admin_client/name"
-          }
-        ]
       end
 
       # takes hash maps with description of missing params and
       # returns array of hash map with key, value for each missed param
 
-      def resolve_missing_params(error_response)
+      def resolve_missing_params(param_list)
         begin
-          error_response ||= @mock_up_response
           user_provided_params, checkup_hash = [], {}
 
           puts "\nPlease fill in missing data.\n"
-          error_response.each do |error|
-
-            string_identifier = error['description'].colorize(:green) + " (#{error['datatype'].upcase})".colorize(:yellow)
+          param_list.each do |param_info|
+            description =
+              if param_info['display_name'] =~ Regexp.new(param_info['description'])
+                param_info['display_name']
+              else 
+                "#{param_info['display_name']} (#{param_info['description']})"
+              end
+            string_identifier = description.colorize(:green) + " (#{param_info['datatype'].upcase})".colorize(:yellow)
 
             puts "Please enter #{string_identifier}:"
             while line = Readline.readline(": ", true)
-              user_provided_params << {:id => error['id'], :value => line}
-              checkup_hash[error['description'].to_sym] = line
+              user_provided_params << {:id => param_info['id'], :value => line}
+              checkup_hash[param_info['description'].to_sym] = line
               break
             end
             
@@ -71,7 +46,7 @@ module DTK
           # make sure this is satisfactory
           while line = Readline.readline("Is provided information ok? (yes|no) ", true)
             # start all over again
-            return resolve_missing_params(error_response) if 'no'.eql? line 
+            return resolve_missing_params(param_list) if 'no'.eql? line 
             # continue with the code
             break if 'yes'.eql? line
           end
