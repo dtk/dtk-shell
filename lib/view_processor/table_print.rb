6bdebe7cf537eb94e1fd21700d0d62655572ec61
Haris KRAJINA <haris@atlantbh.com>
2013-01-03 14:33:07 +0100

    Turned off vertical list

--- lib/view_processor/table_print.rb
+++ lib/view_processor/table_print.rb
@@ -177,7 +177,7 @@ module DTK
       def print
         # hirb print out of our evaluated data in order defined
         # Available options can be viewed here: http://tagaholic.me/hirb/doc/classes/Hirb/Helpers/Table.html#M000008
-        table(@evaluated_data,{:fields => @order_definition,:escape_special_chars => true, :resize => false, :vertical => true })
+        table(@evaluated_data,{:fields => @order_definition,:escape_special_chars => true, :resize => false, :vertical => false })
 
         # in case there were error we print those errors
         unless @error_data.empty?
