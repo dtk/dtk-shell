3e95e4b227afa1d74e3738cde1418d79536868fe
Richard Pelavin <rich.pelavin@gmail.com>
2013-01-03 02:53:44 +0000

    feature_module_restructure: working on push to remote

--- lib/commands/common/thor/push_to_remote.rb
+++ lib/commands/common/thor/push_to_remote.rb
@@ -7,10 +7,15 @@ module DTK::Client
 
     def push_to_remote_aux(module_type,module_id)
       id_field = "#{module_type}_id"
+      path_to_key = SshProcessing.default_rsa_pub_key_path()
+      unless File.file?(path_to_key)
+        raise DtkError,"No File found at (#{path_to_key}). Path is wrong or it is necessary to generate the public rsa key (e.g., run ssh-keygen -t rsa)"
+      end
+      rsa_pub_key = File.open(path_to_key){|f|f.read}
       post_body = {
-        id_field => module_id
+        id_field => module_id,
+        :rsa_pub_key => rsa_pub_key.chomp
       }
-
       response = post(rest_url("#{module_type}/check_remote_auth"),post_body)
       #TODO: stub
       return response
