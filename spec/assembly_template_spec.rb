5eb19b3508d264defc20c04368ee8ed99ac5ec4d
Aldin Milanovic <aldin@atlantbh.com>
2013-01-31 14:23:05 +0100

    rspec changes

--- spec/assembly_template_spec.rb
+++ spec/assembly_template_spec.rb
@@ -1,5 +1,6 @@
 require 'lib/spec_thor'
 require File.expand_path('../lib/require_first', File.dirname(__FILE__))
+require File.expand_path('../lib/commands/thor/assembly_template', File.dirname(__FILE__))
 
 include SpecThor
 
--- spec/state_change_spec.rb
+++ spec/state_change_spec.rb
@@ -1,5 +1,7 @@
 require 'lib/spec_thor'
 require File.expand_path('../lib/require_first', File.dirname(__FILE__))
+require File.expand_path('../lib/commands/thor/state_change', File.dirname(__FILE__))
+
 
 
 
