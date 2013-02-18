1b865b9658062de418306715526190f1820c3069
Amar Trnka <amar@atlantbh.com>
2013-02-18 11:26:20 +0100

    Added SSL communication support - DTK-488

---
+++ lib/bundler_monkey_patch.rb
@@ -0,0 +1,17 @@
+module Bundler
+
+  
+  class << self
+
+
+    def setup(*groups)
+      # DEBUG SNIPPET
+      require 'rubygems'
+      require 'ap'
+      ap "SETUP USAO"
+
+         
+    end
+  end
+
+end
