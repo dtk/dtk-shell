92fc0c6a91faefed34d686c76c9a745bc8927415
Aldin Milanovic <aldin@atlantbh.com>
2013-03-15 12:38:15 +0100

    list-diffs change

--- lib/command_helpers/git_repo.rb
+++ lib/command_helpers/git_repo.rb
@@ -234,8 +234,8 @@ module DTK; module Client; class CommandHelper
         commit_sha = commit_shas[:local_sha]
       end
 
-      diffs = DiffSummary.diff_remote(repo,"remotes/#{remote_branch_ref}")
-      # diffs = DiffSummary.diff(repo,"remotes/#{remote_branch_ref}",local_branch)
+      # diffs = DiffSummary.diff_remote(repo,"remotes/#{remote_branch_ref}")
+      diffs = DiffSummary.diff(repo,"remotes/#{remote_branch_ref}",local_branch)
       commit_sha = repo.find_remote_sha(remote_branch_ref)
       
       {"diffs" => diffs, "commit_sha" => commit_sha, "repo_obj" => repo, "status" => status}
