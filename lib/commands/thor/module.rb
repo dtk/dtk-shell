d506c4554079d1e76d101a41501c515e6fe110c8
Aldin Milanovic <aldin@atlantbh.com>
2013-05-14 09:45:08 +0200

    DTK-669 Get rid of add-access commands in DTK Client for importing modules and services no_build

--- lib/commands/thor/module.rb
+++ lib/commands/thor/module.rb
@@ -498,39 +498,40 @@ module DTK::Client
 
     #TODO: add-direct-access and remove-direct-access should be removed as commands and instead add-direct-access 
     #Change from having module-command/add_direct_access to being a command to being done when client is installed if user wants this option
-    desc "add-direct-access [PATH-TO-RSA-PUB-KEY]","Adds direct access to modules. Optional paramaeters is path to a ssh rsa public key and default is <user-home-dir>/.ssh/id_rsa.pub"
-    def add_direct_access(context_params)
-      path_to_key = context_params.retrieve_arguments([:option_1],method_argument_names)
+    # desc "add-direct-access [PATH-TO-RSA-PUB-KEY]","Adds direct access to modules. Optional paramaeters is path to a ssh rsa public key and default is <user-home-dir>/.ssh/id_rsa.pub"
+    # def add_direct_access(context_params)
+    #   path_to_key = context_params.retrieve_arguments([:option_1],method_argument_names)
 
-      path_to_key ||= SshProcessing.default_rsa_pub_key_path()
-      unless File.file?(path_to_key)
-        raise DTK::Client::DtkError, "No File found at (#{path_to_key}). Path is wrong or it is necessary to generate the public rsa key (e.g., run ssh-keygen -t rsa)"
-      end
-      rsa_pub_key = File.open(path_to_key){|f|f.read}
-      post_body = {
-        :rsa_pub_key => rsa_pub_key.chomp
-      }
-      response = post(rest_url("component_module/add_user_direct_access"),post_body)
-      return response unless response.ok?
-      repo_manager_fingerprint,repo_manager_dns = response.data_ret_and_remove!(:repo_manager_fingerprint,:repo_manager_dns)
-      SshProcessing.update_ssh_known_hosts(repo_manager_dns,repo_manager_fingerprint)
-      response
-    end
+    #   path_to_key ||= SshProcessing.default_rsa_pub_key_path()
+    #   unless File.file?(path_to_key)
+    #     raise DTK::Client::DtkError, "No File found at (#{path_to_key}). Path is wrong or it is necessary to generate the public rsa key (e.g., run ssh-keygen -t rsa)"
+    #   end
+    #   rsa_pub_key = File.open(path_to_key){|f|f.read}
+    #   post_body = {
+    #     :rsa_pub_key => rsa_pub_key.chomp
+    #   }
+    #   response = post(rest_url("component_module/add_user_direct_access"),post_body)
+    #   return response unless response.ok?
+      
+    #   repo_manager_fingerprint,repo_manager_dns = response.data_ret_and_remove!(:repo_manager_fingerprint,:repo_manager_dns)
+    #   SshProcessing.update_ssh_known_hosts(repo_manager_dns,repo_manager_fingerprint)
+    #   response
+    # end
 
-    desc "remove-direct-access [PATH-TO-RSA-PUB-KEY]","Removes direct access to modules. Optional paramaeters is path to a ssh rsa public key and default is <user-home-dir>/.ssh/id_rsa.pub"
-    def remove_direct_access(context_params)
-      path_to_key = context_params.retrieve_arguments([:option_1],method_argument_names)
+    # desc "remove-direct-access [PATH-TO-RSA-PUB-KEY]","Removes direct access to modules. Optional paramaeters is path to a ssh rsa public key and default is <user-home-dir>/.ssh/id_rsa.pub"
+    # def remove_direct_access(context_params)
+    #   path_to_key = context_params.retrieve_arguments([:option_1],method_argument_names)
 
-      path_to_key ||= "#{ENV['HOME']}/.ssh/id_rsa.pub" #TODO: very brittle
-      unless File.file?(path_to_key)
-        raise DTK::Client::DtkError,"No File found at (#{path_to_key}). Path is wrong or it is necessary to generate the public rsa key (e.g., run ssh-keygen -t rsa)"
-      end
-      rsa_pub_key = File.open(path_to_key){|f|f.read}
-      post_body = {
-        :rsa_pub_key => rsa_pub_key.chomp
-      }
-      post rest_url("component_module/remove_user_direct_access"), post_body
-    end
+    #   path_to_key ||= "#{ENV['HOME']}/.ssh/id_rsa.pub" #TODO: very brittle
+    #   unless File.file?(path_to_key)
+    #     raise DTK::Client::DtkError,"No File found at (#{path_to_key}). Path is wrong or it is necessary to generate the public rsa key (e.g., run ssh-keygen -t rsa)"
+    #   end
+    #   rsa_pub_key = File.open(path_to_key){|f|f.read}
+    #   post_body = {
+    #     :rsa_pub_key => rsa_pub_key.chomp
+    #   }
+    #   post rest_url("component_module/remove_user_direct_access"), post_body
+    # end
   end
 end
 
