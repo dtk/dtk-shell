282f97172cd8430fe3445283784e5dfc1ee2b97d
Rich Pelavin <ric@reactor8.com>
2013-05-14 20:35:14 +0000

    ended up handling pull-from-remote by simulating 'git merge -s theirs

--- lib/command_helpers/git_repo.rb
+++ lib/command_helpers/git_repo.rb
@@ -275,7 +275,7 @@ module DTK; module Client; class CommandHelper
       elsif [:branchpoint,:local_ahead].include?(merge_rel)
         # TODO: right now just wiping out what is in repo
         diffs = DiffSummary.diff(repo,"remotes/#{remote_branch_ref}",local_branch)
-        repo.merge(remote_branch_ref,:strategy_option => "theirs")
+        repo.merge_theirs(remote_branch_ref)
         { :diffs => diffs, :commit_sha => repo.head_commit_sha() }
       elsif merge_rel == :local_behind
         #see if any diffs between fetched remote and local branch
