cea8b3376781b105f6328d745f08e10549f08d7d
Rich Pelavin <ric@reactor8.com>
2013-06-09 17:00:27 +0000

    fixed small bug

--- lib/commands/thor/module.rb
+++ lib/commands/thor/module.rb
@@ -483,7 +483,7 @@ module DTK::Client
     def edit(context_params)
       component_module_id = context_params.retrieve_arguments([:module_id!],method_argument_names)
       module_name         = context_params.retrieve_arguments([:module_id],method_argument_names)
-      version             = thor_options["version"]
+      version             = options.version
 
       # if this is not name it will not work, we need module name
       if module_name.to_s =~ /^[0-9]+$/
