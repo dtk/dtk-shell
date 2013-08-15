de40580ebb062c8910efd361c1295d35699e5529
Rich Pelavin <rich@reactor8.com>
2013-08-15 21:05:50 +0000

    Put Dir.chdir in transaction in util/console.rb to avoid having current Dir directory be set to a stale moduel directory, no_build

--- lib/util/console.rb
+++ lib/util/console.rb
@@ -78,66 +78,66 @@ module DTK::Client
         path = path || OsUtil.module_location(module_type)
 
         # we need to change path like this since system call 'cd' is not supported
-        Dir.chdir(path)
-        puts "[NOTICE] You are switching to unix-shell, to path #{path}"
+        Dir.chdir(path) do
+          puts "[NOTICE] You are switching to unix-shell, to path #{path}"
 
-        begin
-          prompt = DTK::Client::OsUtil.colorize("$dtk:unix-shell ", :yellow)
-          Readline.completion_append_character = ""
-          Readline.completion_proc = Proc.new do |str|
-            Dir[str+'*'].grep(/^#{Regexp.escape(str)}/)
-          end
-          while line = Readline.readline("#{prompt}#{Dir.getwd()}>", true)
-            begin
-              line = line.chomp()
-              break if line.eql?('exit')
-              # since we are not able to support cd command due to ruby specific restrictions
-              # we will be using chdir to this.
-              if (line.match(/^cd /))
-                 # remove cd part of command
-                line = line.gsub(/^cd /,'')
-                # Get path
-                path = line.match(/^\//) ? line : "#{Dir.getwd()}/#{line}"
-                # If filepat* with '*' at the end, match first directory and go in it, else try to change original input
-                if path.match(/\*$/)
-                  dirs = Dir[path].select{|file| File.directory?(file)} 
-                  unless dirs.empty?
-                    Dir.chdir(dirs.first)
-                    next
-                  end
-                end
-                # Change directory
-                Dir.chdir(path)
-              elsif line.match(/^dtk-push-changes/)
-                args       = Shellwords.split(line)
-                commit_msg = nil
-
-                unless args.size==1
-                  raise DTK::Client::DtkValidationError, "To push changes to server use 'dtk-push-changes [-m COMMIT-MSG]'" unless (args[1]=="-m" && args.size==3)
-                  commit_msg = args.last
-                end
-
-                response = push_clone_changes_aux(module_type, module_id, version, commit_msg)
-                puts response["data"][:json_diffs]
-                puts "commit_sha: #{response["data"][:commit_sha]}"
-                puts "commit_msg: #{commit_msg}" unless commit_msg.nil?
-              else
-                system(line)        
-              end
-            rescue Exception => e
-              puts e.message
+          begin
+            prompt = DTK::Client::OsUtil.colorize("$dtk:unix-shell ", :yellow)
+            Readline.completion_append_character = ""
+            Readline.completion_proc = Proc.new do |str|
+              Dir[str+'*'].grep(/^#{Regexp.escape(str)}/)
             end
+            while line = Readline.readline("#{prompt}#{Dir.getwd()}>", true)
+              begin
+                line = line.chomp()
+                break if line.eql?('exit')
+                # since we are not able to support cd command due to ruby specific restrictions
+                # we will be using chdir to this.
+                if (line.match(/^cd /))
+                  # remove cd part of command
+                  line = line.gsub(/^cd /,'')
+                  # Get path
+                  path = line.match(/^\//) ? line : "#{Dir.getwd()}/#{line}"
+                  # If filepat* with '*' at the end, match first directory and go in it, else try to change original input
+                  if path.match(/\*$/)
+                    dirs = Dir[path].select{|file| File.directory?(file)} 
+                    unless dirs.empty?
+                      Dir.chdir(dirs.first)
+                      next
+                    end
+                  end
+                  # Change directory
+                  Dir.chdir(path)
+                elsif line.match(/^dtk-push-changes/)
+                  args       = Shellwords.split(line)
+                  commit_msg = nil
+                  
+                  unless args.size==1
+                    raise DTK::Client::DtkValidationError, "To push changes to server use 'dtk-push-changes [-m COMMIT-MSG]'" unless (args[1]=="-m" && args.size==3)
+                    commit_msg = args.last
+                  end
+                  
+                  response = push_clone_changes_aux(module_type, module_id, version, commit_msg)
+                  #TODO: remove; diagnostics puts response["data"][:json_diffs]
+                  #puts "commit_sha: #{response["data"][:commit_sha]}"
+                  #puts "commit_msg: #{commit_msg}" unless commit_msg.nil?
+                else
+                  system(line)        
+                end
+              rescue Exception => e
+                puts e.message
+              end
+            end
+          rescue Interrupt
+            puts ""
+            # do nothing else
           end
-        rescue Interrupt
-          puts ""
-          # do nothing else
+
+          Readline.completion_append_character = dtk_shell_ac_append_char unless dtk_shell_ac_append_char.nil?
+          Readline.completion_proc = dtk_shell_ac_proc unless dtk_shell_ac_proc.nil?
+          puts "[NOTICE] You are leaving unix-shell."
         end
-
-        Readline.completion_append_character = dtk_shell_ac_append_char unless dtk_shell_ac_append_char.nil?
-        Readline.completion_proc = dtk_shell_ac_proc unless dtk_shell_ac_proc.nil?
-        puts "[NOTICE] You are leaving unix-shell."
       end
-
     end
   end
 end
