5200ce0486e7fd7cab5d195e600c0dd4cf43ea4d
Rich Pelavin <rich@reactor8.com>
2013-09-17 20:41:31 +0000

    feature-assembly-module: cleaning up sending assembly branch version for update module from clone

--- lib/commands/common/thor/common.rb
+++ lib/commands/common/thor/common.rb
@@ -16,13 +16,16 @@ module DTK::Client
       end
     end
 
-    def get_workspace_branch_info_post_body(module_type,module_id,version,opts={})
+    def get_workspace_branch_info_post_body(module_type,module_id,version_explicit,opts={})
       id_field = "#{module_type}_id"
       post_body = {
         id_field => module_id
       }
-      post_body.merge!(:version => version) if version
-      if assembly_module = opts[:assembly_module]
+      assembly_module = opts[:assembly_module]
+      if version = version_explicit||(assembly_module && assembly_module[:version])
+        post_body.merge!(:version => version) 
+      end
+      if assembly_module
         post_body.merge!(:assembly_module => true,:assembly_name => assembly_module[:assembly_name])
       end
       post_body
