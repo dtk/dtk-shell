512212fc02f0d3f495ea5ba1d37d1f2ae584fd8a
Rich Pelavin <rich@reactor8.com>
2013-09-29 02:01:08 +0000

    debugging 'refactor of code to purge local clone for better reuse and to be used for assembly modules'

--- lib/commands/common/thor/purge_clone.rb
+++ lib/commands/common/thor/purge_clone.rb
@@ -5,19 +5,23 @@ module DTK::Client
       version = opts[:version]
       opts_module_loc = (opts[:assembly_module] ? {:assembly_module => opts[:assembly_module]} : Hash.new)
       module_location = OsUtil.module_location(module_type,module_name,version,opts_module_loc)
-      pwd = Dir.getwd()
-      if ((pwd == module_location) || (pwd.include?("#{module_location}/")))
-        OsUtil.print("Local directory '#{module_location}' is not deleted because it is your current working directory.", :yellow) 
-        return response
-      end
-
-      #TODO: check role '("#{modules_path}/" != module_location))' is playing
-      FileUtils.rm_rf(module_location) if (File.directory?(module_location) && ("#{base_path}/" != module_location))
-      
+      dirs_to_delete = module_location
       if opts[:delete_all_versions]
-        OsUtil.module_version_locations(module_type,module_name,version,opts).each |path|
-          FileUtils.rm_rf(path) if File.directory?(path)
+        dirs_to_delete += OsUtil.module_version_locations(module_type,module_name,version,opts)
       end
+      response = Response::Ok.new()  
+      pwd = Dir.getwd()
+      dirs_to_delete.each do |dir|
+        if File.directory?(module_location)
+          unless ((pwd == dir) || (pwd.include?("#{dir}/")))
+            FileUtils.rm_rf(module_location) 
+          else
+            OsUtil.print("Local directory '#{module_location}' is not deleted because it is your current working directory.", :yellow) 
+            response = Response::Error.new()
+          end
+        end
+      end
+      response
     end
   end
 end
