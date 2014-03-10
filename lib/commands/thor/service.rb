54a350ef663a76390cc8281f86ff116d397165a4
Bakir <bakir@atlantbh.com>
2014-03-10 15:42:36 +0100

    small change for execute-stats agent support

--- lib/commands/thor/service.rb
+++ lib/commands/thor/service.rb
@@ -140,7 +140,7 @@ TODO: overlaps with different meaning
             ['list-components',"list-components","# List components."]
           ],
           :utils => [
-            ['execute-tests',"execute-tests","# Execute serverspec tests on module components."],
+            ['execute-tests',"execute-tests [--node NODE-NAME] [--component COMPONENT-NAME]","# Execute tests. --node filters results per node, --component filters results per component."],
             ['get-netstats',"get-netstats","# Get netstats."],
             ['get-ps',"get-ps [--filter PATTERN]","# Get ps."],
             ['grep',"grep LOG-PATH NODE-ID-PATTERN GREP-PATTERN [--first]","# Grep log from multiple nodes. --first option returns first match (latest log entry)."],
@@ -634,7 +634,7 @@ TODO: will put in dot release and will rename to 'extend'
     end
 
     # using HIDE_FROM_BASE to hide this command from base context (dtk:/assembly>)
-    desc "HIDE_FROM_BASE execute-tests [--node NODE] [--component COMPONENT_NAME]", "Execute tests. --node filters results per node, --component filters results per component"
+    desc "HIDE_FROM_BASE execute-tests [--node NODE-NAME] [--component COMPONENT-NAME]", "Execute tests. --node filters results per node, --component filters results per component"
     method_option :node, :type => :boolean, :default => false
     method_option :component, :type => :boolean, :default => false
     def execute_tests(context_params)
