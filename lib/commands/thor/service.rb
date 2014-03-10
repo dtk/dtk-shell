834cdf228f550b1fd63f9e826ffcd8d07e35830b
Bakir <bakir@atlantbh.com>
2014-03-10 16:31:14 +0100

    small change for execute-stats agent support

--- lib/commands/thor/service.rb
+++ lib/commands/thor/service.rb
@@ -140,7 +140,7 @@ TODO: overlaps with different meaning
             ['list-components',"list-components","# List components."]
           ],
           :utils => [
-            ['execute-tests',"execute-tests [--component COMPONENT-NAME]","# Execute tests. --component filters results per component."],
+            ['execute-tests',"execute-tests","# Execute tests. --component filters results per component."],
             ['get-netstats',"get-netstats","# Get netstats."],
             ['get-ps',"get-ps [--filter PATTERN]","# Get ps."],
             ['grep',"grep LOG-PATH NODE-ID-PATTERN GREP-PATTERN [--first]","# Grep log from multiple nodes. --first option returns first match (latest log entry)."],
@@ -635,7 +635,7 @@ TODO: will put in dot release and will rename to 'extend'
 
     # using HIDE_FROM_BASE to hide this command from base context (dtk:/assembly>)
     desc "HIDE_FROM_BASE execute-tests [-c COMPONENT-NAME]", "Execute tests. --component filters results per component"
-    method_option :component, :type => :string, :aliases => '-c', :banner => "COMPONENT-NAME", :desc => "execute tests"
+    method_option :component, :aliases => '-c'
     def execute_tests(context_params)
       execute_tests_aux(context_params)
     end
