f2c88584c5e8d3bb8c98851c6c6f0c4b6e07a1a4
Aldin Milanovic <aldin@atlantbh.com>
2014-05-26 16:35:11 +0200

    DTK-1472 When Import Inventory DSL put in parse checks

--- lib/commands/common/thor/inventory_parser.rb
+++ lib/commands/common/thor/inventory_parser.rb
@@ -4,9 +4,10 @@ module DTK::Client
    private
 
     def parse_inventory_file(file_path)
-      hash = YAML.load_file(file_path)
       ssh_creds_path = ::DTK::Client::Configurator::NODE_SSH_CREDENTIALS
-      ssh_creds_data = YAML.load_file(ssh_creds_path)
+      ssh_creds_data = parse_ssh_credentials_file(ssh_creds_path)
+
+      hash = validate_inventory_data(file_path)
 
       ret = Hash.new
       defaults = hash["defaults"]
@@ -15,7 +16,7 @@ module DTK::Client
         display_name = data["name"]||node_name
         ssh_credentials = data["ssh_credentials"]||defaults["ssh_credentials"]
 
-        raise DTK::Client::DtkValidationError, "Credentials for '#{ssh_credentials}' does not exist in credentials file '#{ssh_creds_path}'" unless ssh_creds_data.include?(ssh_credentials)
+        raise DtkValidationError, "Credentials for '#{ssh_credentials}' does not exist in credentials file '#{ssh_creds_path}'" unless ssh_creds_data.include?(ssh_credentials)
         
         ref = "physical--#{display_name}"
         ret[ref] = {
@@ -28,6 +29,50 @@ module DTK::Client
 
       ret
     end
+
+    def parse_ssh_credentials_file(file_path)
+      begin
+        data = YAML.load_file(file_path)
+      rescue SyntaxError => e
+        raise DSLParsing::YAMLParsing.new("YAML parsing error #{e.message} in file", file_path)
+      end
+
+      data.each do |k,v|
+        raise DtkValidationError, "File: '#{file_path}'. Ssh credentials '#{k}' missing required field 'ssh_user'." unless v['ssh_user']
+        raise DtkValidationError, "File: '#{file_path}'. Ssh credentials '#{k}' should contain 'ssh_password' or 'sudo_password'." unless (v['ssh_password'] || v['sudo_password'])
+      end
+
+      data
+    end
+
+    def validate_inventory_data(file_path)
+      begin
+        data = YAML.load_file(file_path)
+      rescue SyntaxError => e
+        raise DSLParsing::YAMLParsing.new("YAML parsing error #{e.message} in file", file_path)
+      end
+
+      defaults = data['defaults']||[]
+      nodes = data['nodes']||[]
+
+      nodes.each do |k,v|
+        os_type = v['os_type']||defaults['os_type']
+        ssh_credentials = v['ssh_credentials']||defaults['ssh_credentials']
+
+        # os_type is required field and should be set through node specific fields or used from defaults
+        raise DtkValidationError, "Missing required field 'os_type' for node '#{k}'." unless os_type
+
+        # ssh_credentials is required field and should be set through node specific fields or used from defaults
+        raise DtkValidationError, "Missing required field 'ssh_credentials' for node '#{k}'." unless ssh_credentials
+
+        # currently we support 'ubuntu', 'centos, 'redhat' and 'debian' as os types and should be set through node specific field
+        # or used from defaults
+        raise DtkValidationError, "Os_type '#{os_type}' is not valid for node '#{k}'. Valid os types: #{ValidOsTypes}." unless ValidOsTypes.include?(os_type)
+      end
+
+      data
+    end
+    ValidOsTypes = ['ubuntu', 'centos', 'redhat', 'debian']
 
   end
 end
