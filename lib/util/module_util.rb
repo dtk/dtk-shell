9e6c9b48a5afe11ffc3ab4ee8fe2a9215d67b288
Rich Pelavin <rich@reactor8.com>
2014-08-27 21:19:19 +0000

    fixed first part of bug DTK-1597

--- lib/util/module_util.rb
+++ lib/util/module_util.rb
@@ -10,6 +10,19 @@ module DTK
         "#{module_namespace}#{NAMESPACE_SEPERATOR}#{module_name}"
       end
 
+      # returns [namespace,name]; namespace can be null if cant determine it
+      def self.full_module_name_parts?(name_or_full_module_name)
+        if name_or_full_module_name.nil?
+          return [nil,nil]
+        end
+        if name_or_full_module_name =~ Regexp.new("(^.+)#{NAMESPACE_SEPERATOR}(.+$)")
+          namespace,name = [$1,$2]
+        else
+          namespace,name = [nil,name_or_full_module_name]
+        end
+        [namespace,name]
+      end
+
       def self.check_format!(module_identifier)
         return module_identifier if module_identifier.match(/^[0-9]+$/)
         raise DtkError, "Module name should be in following format NAMESPACE::MODULE_NAME" unless module_identifier.match(/^.+::.+$/)
