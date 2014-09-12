cae2c2633addff7acd0082c5dc18ff69aa980aa4
Haris Krajina <haris@atlantbh.com>
2014-09-12 12:50:33 +0200

    DEV options to dtk client

--- lib/commands/thor/test_module.rb
+++ lib/commands/thor/test_module.rb
@@ -233,5 +233,19 @@ module DTK::Client
       list_diffs_module_aux(context_params)
     end
 
+
+    #
+    # DEVELOPMENT MODE METHODS
+    #
+    if DTK::Configuration.get(:development_mode)
+      desc "delete-all [-y]","Delete all service modules"
+      def delete_all(context_params)
+        response = list(context_params)
+
+        response.data().each do |e|
+          run_shell_command("delete #{e['display_name']} -y -p")
+        end
+      end
+    end
   end
 end
