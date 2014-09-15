2bb3e82e16dcecc692a4b504d9a1cc24afce2f62
Haris Krajina <haris@atlantbh.com>
2014-09-15 16:11:50 +0200

    Added DTK-1610 setting module in namespace/module_name location

--- lib/util/os_util.rb
+++ lib/util/os_util.rb
@@ -117,7 +117,12 @@ module DTK
               ["#{base_all_types}/#{type}", module_name]
             end
           else
-            [base_path, "#{module_name}#{version && "-#{version}"}"]
+            # we detect if we are using full name
+            if (module_name.match(/(.*):(.*)/))
+              [base_path, "#{$1}", "#{$2}#{version && "-#{version}"}"]
+            else
+              [base_path, "#{module_name}#{version && "-#{version}"}"]
+            end
           end
         end
 
