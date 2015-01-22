2e13d07753b636cde0fd0dc8005d4561341274d8
Haris Krajina <haris@atlantbh.com>
2015-01-22 15:59:59 +0100

    Fixing dtk client

--- lib/commands/thor/service_module.rb
+++ lib/commands/thor/service_module.rb
@@ -7,7 +7,7 @@ dtk_require_from_base("commands/thor/assembly")
 dtk_require_from_base('command_helpers/service_importer')
 dtk_require_common_commands('thor/common')
 dtk_require_common_commands('thor/module')
-
+dtk_require_common_commands('thor/poller')
 
 module DTK::Client
   class ServiceModule < CommandBaseThor
@@ -18,6 +18,7 @@ module DTK::Client
       include ReparseMixin
       include ServiceImporter
       include ModuleMixin
+      include Poller
 
       def get_service_module_name(service_module_id)
         get_name_from_id_helper(service_module_id)
@@ -124,7 +125,7 @@ module DTK::Client
         post_body[:diff] = options.diff? ? options.diff : {}
         post_body.merge!(:module_namespace => options.namespace) if options.namespace
 
-        post rest_url("service_module/#{action}"), post_body
+        response = post rest_url("service_module/#{action}"), post_body
       # If user is on service identifier level, list task can't have '--remote' option.
       else
         # TODO: this is temp; will shortly support this
