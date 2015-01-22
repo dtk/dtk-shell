671d9a48b21c4d46b8975948af43b3f2a08edff3
Haris Krajina <haris@atlantbh.com>
2015-01-22 15:56:40 +0100

    Fixing dtk client

--- lib/commands/thor/service_module.rb
+++ lib/commands/thor/service_module.rb
@@ -145,7 +145,7 @@ module DTK::Client
         end
         response = post rest_url("service_module/#{action}"), { :service_module_id => service_module_id }
       end
-      return response unless response.ok?
+
       response.render_table(data_type) unless response.nil?
 
       response
