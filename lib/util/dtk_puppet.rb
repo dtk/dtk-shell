3b250b895776e7c4989c6681c0246044f7715b1a
Haris Krajina <haris@atlantbh.com>
2015-02-06 13:03:02 +0100

    Minor changes

--- lib/util/dtk_puppet.rb
+++ lib/util/dtk_puppet.rb
@@ -1,8 +1,10 @@
+# TODO-REMOVE: Check if we need this anymore
+
 module DTK
   module Client
     #
     # Class is used as puppet wrapper, at the moment it will use console comamnds, later
-    # to be replaced with direct usage of puppet code, or re-implentation of their direct calls 
+    # to be replaced with direct usage of puppet code, or re-implentation of their direct calls
     #
     class DtkPuppet
 
@@ -14,16 +16,16 @@ module DTK
       # Returns: Name of directory where module is saved
       def self.install_module(module_name)
         output = nil
-        
+
         OsUtil.suspend_output do
           output = `puppet module install #{module_name} --modulepath #{MODULE_PATH} --force --render-as json`
         end
-      
+
         # extract json from output, regex will match json in string
         matched = output.match(/\{.+\}/)
 
         raise DTK::Client::DtkError, "Puppet module '#{module_name}' not found." unless matched
-        
+
         # parse matched json
         result = JSON.parse(matched[0])
 
@@ -34,13 +36,13 @@ module DTK
           filtered = filtered.split(/\n/).map(&:strip).join(', ')
           raise DTK::Client::DtkError, filtered
         end
-        
+
         # puppet uses last part of the module name, as dir for location
         dir_name = module_name.split('-').last
         puts "Successfully installed '#{module_name}' from puppet forge, location: '#{MODULE_PATH}/#{dir_name}'"
         return dir_name
       end
-      
+
     end
   end
 end
