14064b96588dbfec9093d6aaf491010b1997c546
Aldin Milanovic <aldin@atlantbh.com>
2015-02-18 16:18:30 +0100

    DTK-1942 When create an assembly and give message (with -m) message does not get updated in assembly dsl

--- lib/commands/thor/workspace.rb
+++ lib/commands/thor/workspace.rb
@@ -198,9 +198,12 @@ module DTK::Client
       Response::Ok.new()
     end
 
-    desc "WORKSPACE-NAME/ID create-assembly [NAMESPACE:]SERVICE-MODULE-NAME ASSEMBLY-NAME [-p]", "Create a new assembly from the workspace instance in the designated service module."
+    desc "WORKSPACE-NAME/ID create-assembly [NAMESPACE:]SERVICE-MODULE-NAME ASSEMBLY-NAME [-p] [-m DESCRIPTION]", "Create a new assembly from the workspace instance in the designated service module."
    # The option -p will purge the workspace after assembly creation." 
     method_option :purge, :aliases => '-p', :type => :boolean, :default => false
+    method_option "description",:aliases => "-m" ,
+      :type => :string,
+      :banner => "DESCRIPTION"
     def create_assembly(context_params)
       workspace_id, service_module_full_name, assembly_template_name = context_params.retrieve_arguments([:workspace_id!,:option_1!,:option_2!],method_argument_names)
 
@@ -209,7 +212,9 @@ module DTK::Client
       return resp unless resp.ok?
       default_namespace = resp.data
 
-      response = promote_assembly_aux(:create,workspace_id,service_module_full_name,assembly_template_name,{:default_namespace=>default_namespace})
+      opts = {:default_namespace => default_namespace}
+      opts.merge!(:description => options.description) if options.description
+      response = promote_assembly_aux(:create,workspace_id,service_module_full_name,assembly_template_name,opts)
       return response unless response.ok?
 
       if options.purge?
