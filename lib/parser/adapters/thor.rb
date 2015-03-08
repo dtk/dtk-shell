8d5f70aa3a609a341e6b8bc07c6ea33ba9fe95fd
Rich Pelavin <rich@reactor8.com>
2015-03-08 05:31:06 +0000

    fixing bug where namespace 'dtk-meta-user' in add-component gets truncated to meta-user

--- lib/parser/adapters/thor.rb
+++ lib/parser/adapters/thor.rb
@@ -468,7 +468,7 @@ module DTK
           namespace, name = nil, ''
 
           if (component_full_name||'').include?(':')
-            match = component_full_name.match(/(\w*\-?\w*):{1}(.*)/)
+            match = component_full_name.match(/(^[^:]+):{1}(.*$)/)
             namespace, name = [$1,$2]
 
             return [nil, component_full_name] if (name.include?(':') && !name.include?('::'))
