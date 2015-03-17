baf7d9aee4f50e3fc4bf09cb47309f3b7df58da4
Rich Pelavin <rich@reactor8.com>
2015-03-17 01:49:11 +0000

    DTK-1986: code so that can intercept each command in a execute body

--- lib/execute/execute_context.rb
+++ lib/execute/execute_context.rb
@@ -11,28 +11,36 @@ class DTK::Client::Execute
     end
 
     def execute(&block)
+      @proxy = Proxy.new()
       result, command = instance_eval(&block)
-      if @print_results
-        pp(:command => command.input_hash(),:result => result) 
-      end
       result
     end
 
-    # commands in the execute context
-    def post_rest_call(path,body={})
-      command = Command::RestCall::Post.new(:path => path,:body => body)
-      result = CommandProcessor.execute(command)
+    def method_missing(m, *args, &block)
+      result, command = @proxy.send(m, *args, &block)
+      if @print_results
+        pp(:command => command.input_hash(),:result => result) 
+      end
       [result, command]
     end
 
-    def call(object_type__method,params={})
-      object_type, method = split_object_type__method(object_type__method)
-      api_command = Command::APICall.new(:object_type => object_type, :method => method, :params => params)
-      result = nil
-      api_command.executable_commands.each do |command|
-        result = CommandProcessor.execute(command)
+    # commands in the execute context
+    class Proxy < self
+      def call(object_type__method,params={})
+        object_type, method = split_object_type__method(object_type__method)
+        api_command = Command::APICall.new(:object_type => object_type, :method => method, :params => params)
+        result = nil
+        api_command.executable_commands.each do |command|
+          result = CommandProcessor.execute(command)
+        end
+        [result, api_command]
       end
-      [result, api_command]
+      
+      def post_rest_call(path,body={})
+        command = Command::RestCall::Post.new(:path => path,:body => body)
+        result = CommandProcessor.execute(command)
+        [result, command]
+      end
     end
 
     private 
