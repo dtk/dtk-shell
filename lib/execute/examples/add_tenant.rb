d5f70f7bb698d8de9f8257721ce4d7c47a78359c
Rich Pelavin <rich@reactor8.com>
2015-04-02 00:17:02 +0000

    updated the dtk-execute logic

--- lib/execute/examples/add_tenant.rb
+++ lib/execute/examples/add_tenant.rb
@@ -1,12 +1,19 @@
 class DTK::Client::Execute
 
-  def self.add_tenant(tenant_name,params={})
+  def self.add_tenant(tenant_name,catalog_username,params={})
     component_with_namespace = "dtk-meta-user:dtk_tenant[#{tenant_name}]"
     component_namespace, component = (component_with_namespace =~ /(^[^:]+):(.+$)/; [$1,$2])
-    service = 'dtkhost5'
-    node = 'server'
-    tenant_password = params[:tenant_password] || 'foo'
-    catalog_user_name = params[:catalog_user_name] || tenant_name
+    service = params[:service_instance] || 'dtkhost5'
+    node = params[:node_name] || 'server'
+    unless password = params[:password]
+      raise ErrorUsage.new("Password is manditory; use -p commadn line option")
+    end
+
+    av_pairs = {
+      :catalog_username => catalog_username,
+      :tenant_password  => password,
+      :catalog_password => password
+    }
 
     ExecuteContext(:print_results => true) do
       result = call 'service/add_component',
@@ -15,17 +22,14 @@ class DTK::Client::Execute
         :component             => component,
         :namespace             => component_namespace,
         :donot_update_workflow => true
-      
-      result = call 'service/set_attribute',
-        :service        => service,
-        :attribute_path => "#{node}/#{component}/tenant_password",
-        :value          => tenant_password
 
-      result = call 'service/set_attribute',
-        :service        => service,
-        :attribute_path => "#{node}/#{component}/catalog_user_name",
-        :value          => catalog_user_name
-
+      av_pairs.each_pair do |a,v|
+        result = call 'service/set_attribute',
+          :service        => service,
+          :attribute_path => "#{node}/#{component}/#{a}",
+          :value          => v
+      end
+  
       ['dtk_postgresql::databases'].each do |shared_service_component|
         result = call 'service/link_components',
           :service          => service,
@@ -37,7 +41,6 @@ class DTK::Client::Execute
         :service         => service,
         :workflow_name   => 'add_tenant',
         :workflow_params => {'name' => tenant_name}
-        
 
     end
   end
