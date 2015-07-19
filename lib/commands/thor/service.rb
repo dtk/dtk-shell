727738eca125348492dbf473e17e8de17e28f7ca
Rich Pelavin <rich.reactor8.com>
2015-07-19 18:40:18 +0000

    moved task_status code directly under lib

--- lib/commands/thor/service.rb
+++ lib/commands/thor/service.rb
@@ -2,10 +2,10 @@ require 'rest_client'
 require 'json'
 require 'colorize'
 
-dtk_require_from_base("dtk_logger")
-dtk_require_from_base("util/os_util")
-dtk_require_from_base("command_helper")
-dtk_require_common_commands('thor/task_status')
+dtk_require_from_base('dtk_logger')
+dtk_require_from_base('util/os_util')
+dtk_require_from_base('command_helper')
+dtk_require_from_base('task_status')
 dtk_require_common_commands('thor/set_required_attributes')
 dtk_require_common_commands('thor/edit')
 dtk_require_common_commands('thor/purge_clone')
