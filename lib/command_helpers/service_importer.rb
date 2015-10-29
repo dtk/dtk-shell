7980ed0d836cdc16c0fa311e9f39958d0dfcbd59
Aldin Milanovic <aldin@atlantbh.com>
2015-10-29 21:45:36 +0100

    Install master version when not installed

--- lib/command_helpers/service_importer.rb
+++ lib/command_helpers/service_importer.rb
@@ -134,7 +134,7 @@ module DTK::Client
         else
           # import from Repo Manager
           new_context_params = ::DTK::Shell::ContextParams.new([module_name])
-          new_context_params.override_method_argument!('option_2', module_version)
+          new_context_params.override_method_argument!('option_2', version) if version && !version.eql?('master')
           new_context_params.forward_options(:skip_cloning => false, :skip_auto_install => true, :module_type => module_type).merge!(opts)
           response = ContextRouter.routeTask(module_type, 'install', new_context_params, @conn)
         end
--- lib/commands/thor/service_module.rb
+++ lib/commands/thor/service_module.rb
@@ -135,7 +135,7 @@ module DTK::Client
         post_body = (options.remote? ? { :rsa_pub_key => SSHUtil.rsa_pub_key_content() } : {:detail_to_include => ["remotes"]})
         post_body[:diff] = options.diff? ? options.diff : {}
         post_body.merge!(:module_namespace => options.namespace) if options.namespace
-        post_body[:detail_to_include] << 'versions' if options.with_versions?
+        post_body[:detail_to_include] << 'versions'# if options.with_versions?
 
         response = post rest_url("service_module/#{action}"), post_body
       # If user is on service identifier level, list task can't have '--remote' option.
